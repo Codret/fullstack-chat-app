d6382eb660f2dabf786ac2fbd2534059f3b85b11
codret <akkikeraste@gmail.com>
2025-06-30 17:15:10 +0530

    Fixed socket BASE_URL issue and environment variables

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,11 +29,11 @@ const  __dirname = path.resolve();
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoute)
 
-if(process.env.NODE_ENV === "production ") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
     app.get("*", (req,res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
     })
 }
 
