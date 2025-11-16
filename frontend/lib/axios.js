44ce507b09b4010464e41855703e0300cf689bfe
Akash <akkikeraste@gmail.com>
2025-11-16 14:14:08 +0530

    fixes other issues

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+axios.defaults.withCredentials = true;
 
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.MODE ==="development"? "http://localhost:5001/api" : "/api",
