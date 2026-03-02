130ef6f61952766d7c722f68582f4e6d77a58a2f
Akash M <akkikeraste@gmail.com>
2026-03-02 12:51:52 +0530

    Refactor axios configuration and import statements

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -1,10 +1,16 @@
-import axios from 'axios';
-axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
-axios.defaults.withCredentials = true;
+// import axios from 'axios';
+// axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+// axios.defaults.withCredentials = true;
+
+// export const axiosInstance = axios.create({
+//     baseURL: import.meta.env.MODE ==="development"? "http://localhost:5001/api" : "/api",
+//     // baseURL: "https://fullstack-chat-app-uyn3.onrender.com",
+//     withCredentials: true,
+// })
+
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-    // baseURL: import.meta.env.MODE ==="development"? "http://localhost:5001/api" : "/api",
-    baseURL: "https://fullstack-chat-app-uyn3.onrender.com",
-    withCredentials: true,
-})
-
+  baseURL: "https://fullstack-chat-app-uyn3.onrender.com",
+  withCredentials: true,
+});
