d8dff1ce264d178d8b45b0edb89f6f6246c74cd5
Akash M <akkikeraste@gmail.com>
2026-03-02 12:54:15 +0530

    Update axios baseURL to include '/api' path

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -11,6 +11,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://fullstack-chat-app-uyn3.onrender.com",
+  baseURL: "https://fullstack-chat-app-uyn3.onrender.com/api",
   withCredentials: true,
 });
