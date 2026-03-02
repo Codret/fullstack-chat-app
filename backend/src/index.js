950ef69a3b7377a966dad938aef368f07eae3b62
Akash M <akkikeraste@gmail.com>
2026-03-02 12:38:25 +0530

    Update CORS origin to production URL

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    // origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "https://fullstack-chat-app-uyn3.onrender.com",
     credentials: true,
   })
 );
