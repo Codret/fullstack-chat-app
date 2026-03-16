44ed12d50f1c5a7f487896e7a2e22b4ccae64c81
Codret <akkikeraste@gmail.com>
2026-03-16 17:35:52 +0530

    added features for frontend add

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -2,8 +2,15 @@ import axios from 'axios';
 axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 axios.defaults.withCredentials = true;
 
+// export const axiosInstance = axios.create({
+//     baseURL: import.meta.env.MODE ==="development"? "http://localhost:5001/api" : "/api",
+//     withCredentials: true,
+// })
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE ==="development"? "http://localhost:5001/api" : "/api",
-    withCredentials: true,
-})
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "https://fullstack-chat-app-uyn3.onrender.com/api",
+  withCredentials: true,
+});
 
